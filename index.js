9f5f503bd695e9193620c603a89f7ed237d7079d
MayuriAI <129197059+MayuriAI@users.noreply.github.com>
2023-03-28 21:01:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('I have completed Master in Computer Application and BSC');
+    res.send('I have completed Master in Computer Application and BSC-IT');
 });
 
 app.listen(3000, () => {
