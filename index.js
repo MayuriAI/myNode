4f7541b26b0629de22a0ad34cfce6a38b144cd11
Mayu Kale <129197059+MayuriAI@users.noreply.github.com>
2023-03-28 15:21:53 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('I have completed Master in Computer Application and BSC');
+});
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
